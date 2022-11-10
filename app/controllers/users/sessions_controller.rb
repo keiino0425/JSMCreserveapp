803c988c63ab52cc5efdd789b1d716ef8fc0e331
keiino0425 <keiino0425@gmail.com>
2022-11-10 16:36:29 +0900

    ログインした後にマイページにリダイレクトされるように設定

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,10 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  def after_sign_in_path_for(_resource)
+    user_path(:id)
+  end
+
   # GET /resource/sign_in
   # def new
   #   super
