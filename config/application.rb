a91ed0121cf896d2432115860f525cecd16cc689
keiino0425 <keiino0425@gmail.com>
2022-11-10 15:43:07 +0900

    ルーティングを整理

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,10 @@ module JSMCreserveapp
     config.i18n.default_locale = :ja
     # タイムゾーンの変更（例)created_at カラムを取り出したときに日本時間に変換されるようになる）
     config.time_zone = 'Asia/Tokyo'
+    initializer(:remove_action_mailbox_and_activestorage_routes, after: :add_routing_paths) { |app|
+     app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}
+     app.routes_reloader.paths.delete_if {|path| path =~ /actionmailbox/ }
+    }
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
