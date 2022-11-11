fbcb029e4e0d1f8a6ed366bf70a3ad1dbd7786c9
keiino0425 <keiino0425@gmail.com>
2022-11-11 17:39:34 +0900

    講師のログインフォーム周りを設定

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   resources :users
+  resources :teachers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
