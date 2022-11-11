55026fbaf823a463c101cf1783734669cc8f3b79
keiino0425 <keiino0425@gmail.com>
2022-11-12 03:25:28 +0900

    予約画面の前に講師を選ぶ画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   resources :users
   resources :teachers
   resources :reservations
+  get 'users/:id/choice', to: 'users#choice'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
