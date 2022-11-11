3eafcfc4b749aad8dfeaae3adc19c022950969d7
keiino0425 <keiino0425@gmail.com>
2022-11-12 01:56:51 +0900

    カレンダーを表示

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   }
   resources :users
   resources :teachers
+  resources :reservations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
