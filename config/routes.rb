ba93fe9d3779b3fe54b1fef12ec2d8b518343a8b
keiino0425 <keiino0425@gmail.com>
2022-11-18 14:55:27 +0900

    講師が自分のスケジュールを予約できるよう設定の途中

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,10 @@ Rails.application.routes.draw do
     end
     get '/choice', to: 'reservations#choice'
   end
+  resources :teachers do
+    get '/reservations/index', to: 'reservations#teacher_index'
+    post '/reservations/index', to: 'reservations#teacher_create'
+    get '/reservations/new', to: 'reservations#teacher_new'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
