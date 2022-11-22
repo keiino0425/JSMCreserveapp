55bace039c99b55c046cdd19e1dd445631919045
keiino0425 <keiino0425@gmail.com>
2022-11-22 17:31:40 +0900

    講師のマイページを設定

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,5 +2,7 @@ class TeachersController < ApplicationController
   before_action :authenticate_teacher!
   
   def show
+    @teacher = current_teacher
+    @teacher_reservations = @teacher.reservations.where("user_id IS NOT NULL").where("start_time >= ?", DateTime.current).order(start_time: :desc)
   end
 end
--- app/helpers/teachers_helper.rb
+++ app/helpers/teachers_helper.rb
@@ -1,2 +1,6 @@
 module TeachersHelper
+  def check_user(reservation)
+    user = User.find(reservation.user_id)
+    return user.user_name
+  end
 end
