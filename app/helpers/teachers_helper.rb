f13b3c9d6593eb12778f99d1b68244f22f56547b
keiino0425 <keiino0425@gmail.com>
2022-12-01 11:37:20 +0900

    コードを簡略化

--- app/helpers/teachers_helper.rb
+++ app/helpers/teachers_helper.rb
@@ -1,11 +1,11 @@
 module TeachersHelper
   def check_user(reservation)
     user = User.find(reservation.user_id)
-    return user.user_name
+    user.user_name
   end
 
   def user_address(reservation)
     user = User.find(reservation.user_id)
-    return user.user_address
+    user.user_address
   end
 end
