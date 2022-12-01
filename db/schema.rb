406c0f62f6825ffc6419379f1f233dc6fb69cc00
keiino0425 <keiino0425@gmail.com>
2022-12-01 13:42:04 +0900

    仮予約機能のために仮予約モデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_01_013937) do
+ActiveRecord::Schema.define(version: 2022_12_01_043628) do
 
   create_table "reservations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id"
@@ -36,6 +36,16 @@ ActiveRecord::Schema.define(version: 2022_12_01_013937) do
     t.string "teacher_address", null: false
     t.index ["email"], name: "index_teachers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
+  end
+
+  create_table "temp_reservations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "teacher_id", null: false
+    t.datetime "start_time", null: false
+    t.datetime "end_time", null: false
+    t.boolean "address_select", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
