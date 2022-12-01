bfcf677ea81d763993883ac0a04af49c29bca923
keiino0425 <keiino0425@gmail.com>
2022-12-01 10:12:39 +0900

    生徒、講師それぞれにカリキュラム開催場所の住所を入力するカラムを設定

---
+++ db/migrate/20221201004250_add_useraddress_to_users.rb
@@ -0,0 +1,5 @@
+class AddUseraddressToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :user_address, :string, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_21_144028) do
+ActiveRecord::Schema.define(version: 2022_12_01_004525) do
 
   create_table "reservations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_11_21_144028) do
     t.string "teacher_name", null: false
     t.string "teacher_area", null: false
     t.string "teacher_img", null: false
+    t.string "teacher_address", null: false
     t.index ["email"], name: "index_teachers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
   end
@@ -49,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_11_21_144028) do
     t.string "curriculum", null: false
     t.integer "curriculum_num", default: 1, null: false
     t.boolean "video_available", default: true, null: false
+    t.string "user_address", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
