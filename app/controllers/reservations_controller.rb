381af1688f7c7c3a3e8010a18075fd5fed5d7dfc
keiino0425 <keiino0425@gmail.com>
2022-12-12 18:52:04 +0900

    メールの送り相手ごとにmailerを変更

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -74,7 +74,7 @@ class ReservationsController < ApplicationController
     @teacher = Teacher.find(@reservation.teacher_id)
     @start_time = @reservation.start_time
     if @reservation.destroy
-      UserMailer.with(user: @user, teacher: @teacher, start_time: @start_time).reservation_delete_email.deliver_later
+      TeacherMailer.with(user: @user, teacher: @teacher, start_time: @start_time).reservation_delete_email.deliver_later
       flash[:success] = "予約を削除しました。"
       redirect_to user_path(current_user.id)
     else
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,18 +7,4 @@ class UserMailer < ApplicationMailer
     @teacher = Teacher.find(@reservation.teacher_id)
     mail(to: @user.email, subject: '予約が確定しました')
   end
-
-  def reservation_delete_email
-    @user = params[:user]
-    @teacher = params[:teacher]
-    @start_time = params[:start_time]
-    mail(to: @teacher.email, subject: '予約が削除されました')
-  end
-
-  def temp_reservation_email
-    @temp_reservation = params[:temp_reservation]
-    @user = User.find(@temp_reservation.user_id)
-    @teacher = Teacher.find(@temp_reservation.teacher_id)
-    mail(to: @teacher.email, subject: '仮予約が登録されました')
-  end
 end
