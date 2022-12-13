df6901a093fee7394c33ecaaabcc0385deb4f417
keiino0425 <keiino0425@gmail.com>
2022-12-14 02:35:36 +0900

    仮予約を予約する際、自分の他の仮予約or予約が1週間以内にあれば予約不可になるバリデーションを追加

--- app/models/temp_reservation.rb
+++ app/models/temp_reservation.rb
@@ -2,7 +2,25 @@ class TempReservation < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :teacher
   has_one :reservation
-  def self.check_reservation_day(start_time)
+
+  def self.check_reservation_day(user_id, start_time)
+    same_user_temp_reservation = TempReservation.where(user_id: user_id)
+    same_user_reservation = Reservation.where(user_id: user_id)
+    temp_start = same_user_temp_reservation.map(&:start_time)
+    start = same_user_reservation.map(&:start_time)
+
+    temp_start.each do |i|
+      if start_time < (i + 8.days)
+        return "仮予約との間隔が1週間以内のため予約できません。"
+      end
+    end
+
+    start.each do |i|
+      if start_time < (i + 8.days)
+        return "予約との間隔が1週間以内のため予約できません。"
+      end
+    end
+
     if start_time < Date.current
       return "過去の日付は選択できません。正しい日付を選択してください。"
     elsif start_time < (Date.current + 1)
