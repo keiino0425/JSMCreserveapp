5cce5b0ce216eee13ad38800ad08a789ca00e882
keiino0425 <keiino0425@gmail.com>
2022-12-19 08:39:04 +0900

    active_adminのcssが全体に効いていた問題を修正

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Jsm Creserveapp"
+  config.site_title = "JSMCCreserveapp"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -215,6 +215,11 @@ ActiveAdmin.setup do |config|
   #
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
+  config.clear_stylesheets!
+  config.register_stylesheet 'active_admin'
+
+  config.clear_javascripts!
+  config.register_javascript 'active_admin'
 
   # == CSV options
   #
