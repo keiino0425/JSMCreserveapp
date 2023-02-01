a9dc8ea45aea24facf993e4e0c939ee423c43bf4
keiino0425 <keiino0425@gmail.com>
2023-02-02 06:52:50 +0900

    userファクトリを作成し、それが有効であるかテストした

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  # 有効なファクトリを持つこと
+  it "has a valid factory" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
   # メール、名前、エリア、カリキュラム、動画の権限の有無があれば有効な状態であること
   it "is valid with an email, name, area, curriculum, and video_available"
   # メールアドレスがなければ無効な状態であること
