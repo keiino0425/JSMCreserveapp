95b6ee2b474a60c7835cd0e8c9892c4948efb30e
keiino0425 <keiino0425@gmail.com>
2023-02-02 07:58:12 +0900

    userモデルの不必要なカラムを削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,7 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "tester#{n}@example.com" }
     user_name {"Aaron Sumner"}
-    user_area { "近畿" }
     curriculum { "ヘッドスパ" }
-    curriculum_num { 1 }
-    video_available { true }
     user_address { "大阪府大阪市中央区大阪城１−１" }
     password { "dottle-nouveau-pavilion-tights-furze" }
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,14 +11,8 @@ RSpec.describe User, type: :model do
   it "is invalid without an email address"
   # 名がなければ無効な状態であること
   it "is invalid without a name"
-  # エリアがなければ無効な状態であること
-  it "is invalid without an area"
   # カリキュラム内容がなければ無効な状態であること
   it "is invalid without a curriculum"
-  # カリキュラム番号がなければ無効な状態であること
-  it "is invalid without a curriculum_num"
-  # 動画の権限の有無がなければ無効な状態であること
-  it "is invalid without a video_available"
   # 住所がなければ無効な状態であること
   it "is invalid without an address"
   # 重複したメールアドレスなら無効な状態であること
