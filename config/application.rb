81d14b0e72566ad91b508ed0f8a092766542afed
keiino0425 <keiino0425@gmail.com>
2023-02-02 06:10:15 +0900

    factorybotをインストールし、今後モデルを作成した際に自動でファクトリが作成されるよう設定

--- config/application.rb
+++ config/application.rb
@@ -45,11 +45,9 @@ module JSMCreserveapp
 
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false
-      g.factory_bot false
     end
   end
 end
