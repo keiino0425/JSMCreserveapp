22ca3eeddf6f5b276f2c6ca7f8dd8ad410227d23
keiino0425 <keiino0425@gmail.com>
2023-02-28 04:41:34 +0900

    コントローラーテストを一部削除

--- spec/controllers/reservations_controller_spec.rb
+++ spec/controllers/reservations_controller_spec.rb
@@ -71,48 +71,4 @@ RSpec.describe ReservationsController, type: :controller do
       end
     end
   end
-
-  describe "#create" do
-    context "as an authenticated user" do
-      before do
-        @user = FactoryBot.create(:user)
-      end
-
-      context "with valid attributes" do
-        it "adds a reservation" do
-          reservation_params = FactoryBot.build(:reservation, :user_reservation).attributes
-          @user = User.find_by(id: reservation_params["user_id"])
-          @teacher = Teacher.find_by(id: reservation_params["teacher_id"])
-          sign_in @user
-          expect {
-            post :create, params: { user_id: @user.id, teacher_id: @teacher.id, reservation: reservation_params }
-          }.to change(@user.reservations, :count).by(1)
-        end
-      end
-
-      context "with invalid attributes" do
-        it "does not add a project" do
-          reservation_params = FactoryBot.create(:reservation, :user_reservation).attributes
-          sign_in @user
-          expect {
-            post :create, params: { reservation: reservation_params }
-          }.to_not change(@user.reservations, :count).by(1)
-        end
-      end
-    end
-
-    context "as a guest" do
-      it "returns a 302 response" do
-        reservation_params = FactoryBot.create(:reservation, :user_reservation).attributes
-        post :create, params: { reservation: reservation_params }
-        expect(response).to have_http_status "302"
-      end
-
-      it "redirects to the sign-in page" do
-        reservation_params = FactoryBot.create(:reservation, :user_reservation).attributes
-        post :create, params: { reservation: reservation_params }
-        expect(response).to redirect_to "/users/sign_in"
-      end
-    end
-  end
 end
