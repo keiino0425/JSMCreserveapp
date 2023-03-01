5b4dc174ec97da127e64c2d5dfa114c2bdc7af26
keiino0425 <keiino0425@gmail.com>
2023-03-01 16:41:54 +0900

    仮予約を確定orキャンセルする際のシステムスペックを作成

--- spec/system/temp_reservations_spec.rb
+++ spec/system/temp_reservations_spec.rb
@@ -25,4 +25,55 @@ RSpec.describe "TempReservations", type: :system do
     }.to change(user.temp_reservations, :count).by(1)
   end
 
+  #講師が仮予約を確定させる
+  scenario "teacher confirms temp reservation" do
+    user = FactoryBot.create(:user)
+    teacher = FactoryBot.create(:teacher)
+    temp_reservation = FactoryBot.create(:temp_reservation, user_id: user.id, teacher_id: teacher.id)
+
+    visit new_teacher_session_path
+    fill_in "Eメール", with: teacher.email
+    fill_in "パスワード", with: teacher.password
+    click_button "ログイン"
+
+    expect {
+      click_link "確定"
+      click_button "予約する"
+    }.to change(teacher.reservations, :count).by(1), change(teacher.temp_reservations, :count).by(-1)
+  end
+
+  #講師が仮予約をキャンセルする
+  scenario "teacher cancels temp reservations" do
+    user = FactoryBot.create(:user)
+    teacher = FactoryBot.create(:teacher)
+    temp_reservation = FactoryBot.create(:temp_reservation, user_id: user.id, teacher_id: teacher.id)
+
+    visit new_teacher_session_path
+    fill_in "Eメール", with: teacher.email
+    fill_in "パスワード", with: teacher.password
+    click_button "ログイン"
+
+    expect {
+      click_button "削除"
+      expect(page).to have_content "仮予約を削除しました。"
+    }.to change(teacher.temp_reservations, :count).by(-1)
+  end
+
+  #生徒が仮予約をキャンセルする
+  scenario "user canceles temp reservations" do
+    user = FactoryBot.create(:user)
+    teacher = FactoryBot.create(:teacher)
+    temp_reservation = FactoryBot.create(:temp_reservation, user_id: user.id, teacher_id: teacher.id)
+
+    visit new_user_session_path
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: user.password
+    click_button "ログイン"
+
+    expect {
+      save_page
+      click_button "削除"
+      expect(page).to have_content "仮予約を削除しました。"
+    }.to change(user.temp_reservations, :count).by(-1)
+  end
 end
