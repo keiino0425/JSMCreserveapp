73d2d29d36b4cd11fbc00e07b85dc1323c48a888
keiino0425 <keiino0425@gmail.com>
2023-03-03 01:14:18 +0900

    ゲストユーザーとしてログインできる機能を作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,16 @@ class User < ApplicationRecord
   validates :email, :user_name, :curriculum, :user_address, presence: true
   validates :email, uniqueness: true
 
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.user_name = "ゲストユーザー"
+      user.curriculum = "ドライヘッドスパ"
+      user.user_address = "大阪府大阪市中央区大阪城１−１"
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+    end
+  end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
